7dd2a44eeda37db2847bab9de95a6e47d61be886
NataD81 <dvoryanchuk81@gmail.com>
2024-08-18 15:01:09 +0300

    ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import s from "./ContactsPage.module.css";
+import { fetchContacts } from "../../redux/contacts/operations";
+import { selectLoading, selectError } from "../../redux/contacts/selectors";
 
-const ContactsPage = () => (
-  <div className={s.container}>
-    <h1>Contacts</h1>
-    <ContactForm />
-    <ContactList />
-  </div>
-);
+const ContactsPage = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+  return (
+    <div className={s.container}>
+      <h1>Contacts</h1>
+      <ContactForm />
+      {isLoading && <p>Loading contacts...</p>}
+      {error && <p className={s.error}>Error: {error}</p>}
+      <ContactList />
+    </div>
+  );
+};
 
 export default ContactsPage;
